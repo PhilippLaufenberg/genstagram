8d9676ddcfdb409aaf5fe9e459a14b468dd23846
Phil Laufenberg <hello@phillaufenberg.com>
2024-04-04 21:34:41 +0200

    initial commit (?)

---
+++ README.md
@@ -0,0 +1,3 @@
+Course: https://www.youtube.com/watch?v=_W3R2VwRyF4&t=327s
+
+Github with configs: https://gist.github.com/adrianhajdin/4d2500bf5af601bbd9f4f596298d33ac
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./global.css";
+
+const App = () => {
+  return (
+    <h1 className="bg-dark-1 text-3xl font-bold underline">Hello Gengram!</h1>
+  );
+};
+
+export default App;
