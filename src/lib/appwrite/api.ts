56db0e10cadf29e0995af97b25e071334e41ff02
Phil Laufenberg <hello@phillaufenberg.com>
2024-04-06 21:38:13 +0200

    install tailwind and add database IDs and addUserWIP

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { appwriteAccount } from "./config";
+import { appwriteAccount, appwriteAvatars } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,8 +10,27 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
+
+    if (!newAccount) throw new Error("Could not create user account");
+    const avatarUrl = appwriteAvatars.getInitials(user.name);
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
     return newAccount;
   } catch (error) {
     console.error(error);
   }
 }
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {}
--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -4,6 +4,11 @@ export const appwriteConfig = {
   // TS will complain about this, so need to add vite-env.d.ts to src folder
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
+  database: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  saveCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
 export const appwriteClient = new Client();
